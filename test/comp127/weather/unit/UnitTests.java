b834eae051dca74523c5532bef53c489408d4d8e
Lucy Tran <ltran2@macalester.edu>
2023-02-15 13:17:14 -0600

    Updated the LocationDropdownTest

--- test/comp127/weather/unit/UnitTests.java
+++ test/comp127/weather/unit/UnitTests.java
@@ -4,14 +4,20 @@ import comp127.weather.api.ForecastConditions;
 import comp127.weather.widgets.ForecastBox;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.time.*;
 import java.util.Date;
+import java.util.List;
 
 public class UnitTests {
+//   private static WeatherProgram weatherProgram = new WeatherProgram(600);
+
    @Test
-   @DisplayName(" test")
+   @DisplayName("Forecast Box")
    void ForecastBoxTest(){
       LocalDateTime randomTimeOfDay = LocalDateTime.now().plusHours(5);
 //      Date predictionTime = toDate(randomTimeOfDay);
@@ -25,14 +31,26 @@ public class UnitTests {
    }
 
    @Test
-   @DisplayName("Test Location Change")
-   void LocationDropdownTest() {
-      WeatherProgram weatherProgram = new WeatherProgram(600);
-//      weatherProgram.updateLocation("Tokyo");
+   @DisplayName("Location Change")
+   @ParameterizedTest
+   @ValueSource(strings = {"Tokyo"})
+   void LocationDropdownTest(String inputCity) {
+//      List<String> widgetsCities = weatherProgram.updateLocation(inputCity);
+//      for (String widgetCity: widgetsCities) {
+//         System.out.println(widgetCity);
+//         assertEquals(widgetCity, inputCity);
+//      }
    }
 
-   public static void main(String[] args) {
+//   public static void main(String[] args) {
+   @Test
+  void cityTest(){
       WeatherProgram weatherProgram = new WeatherProgram(600);
-      weatherProgram.updateLocation("Tokyo");
 
+      List<String> widgetsCities = weatherProgram.updateLocation("Tokyo");
+      System.out.println("Length of list: "+widgetsCities.size());
+      for (String widgetCity: widgetsCities) {
+         System.out.println("Widget city: " + widgetCity);
+         assertEquals(widgetCity, "Tokyo");
+      }
    }}
